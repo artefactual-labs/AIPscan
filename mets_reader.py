52202f38d02f36dfa4f087b8f4bd36e387d37637
Peter Van Garderen <peter@Peters-Air.hitronhub.home>
2020-03-20 20:25:42 -0700

    add preservation copy information

--- mets_reader.py
+++ mets_reader.py
@@ -1,18 +1,33 @@
 import metsrw
 
 mets = metsrw.METSDocument.fromfile(
-    "downloads/2020-03-19--19:54:46/1e11edbd-d19c-4afa-9ac4-cd656f675f68.xml"
+    "downloads/2020-03-19--19:54:46/3706e0f8-5ab5-4a26-8927-2a7796a24043.xml"
 )
-
-for file in mets.all_files():
-    if file.use == "original":
-        print("original: " + file.label + "\n" + "uuid: " + file.file_uuid)
-        for premis_object in file.get_premis_objects():
-            print("format: " + str(premis_object.format_name))
-            if str(premis_object.format_version) != "(('format_version',),)":
-                print("version: " + str(premis_object.format_version))
-            print("\n")
 
 # f = open("files.txt", "w")
 # f.write(str(mets.all_files()))
 # f.close()
+
+for file in mets.all_files():
+    if file.use == "original":
+        print("original file: " + file.label + "\n" + "file uuid: " + file.file_uuid)
+        for premis_object in file.get_premis_objects():
+            print("file format: " + str(premis_object.format_name))
+            if str(premis_object.format_version) != "(('format_version',),)":
+                print("version: " + str(premis_object.format_version))
+            if str(premis_object.related_object_identifier_value) != "()":
+                print(
+                    "preservation copy uuid: "
+                    + str(premis_object.related_object_identifier_value)
+                )
+            print("\n")
+
+    if file.use == "preservation":
+        print(
+            "preservation copy: " + file.label + "\n" + "file uuid: " + file.file_uuid
+        )
+        for premis_object in file.get_premis_objects():
+            print("file format: " + str(premis_object.format_name))
+            if str(premis_object.format_version) != "(('format_version',),)":
+                print("version: " + str(premis_object.format_version))
+            print("\n")
