367655ab6438fc5031b201d7070b0e8fec156281
Peter Van Garderen <peter@Peters-MacBook-Air.local>
2020-04-11 15:23:00 -0700

    add CLI versions of METS fetch and parse scripts

---
+++ AIPscan/CLI_am_ss_requests.py
@@ -0,0 +1,130 @@
+__author__ = "Peter Van Garderen"
+__email__ = "peter@artefactual.com"
+
+import requests
+import os.path
+from datetime import datetime
+
+baseUrl = ""
+username = ""
+apiKey = ""
+apiCommand = "/api/v2/file/"
+limit = "20"
+offset = "0"
+totalAIPs = 0
+
+
+def get_packages(nextUrl):
+    """
+    request package metadata from Archivematica Storage Service
+    """
+
+    if nextUrl is not None:
+        packages_response = requests.get(baseUrl + nextUrl)
+    else:
+        packages_response = requests.get(
+            baseUrl
+            + apiCommand
+            + "?limit="
+            + limit
+            + "&offset="
+            + offset
+            + "&username="
+            + username
+            + "&api_key="
+            + apiKey
+        )
+
+    return packages_response.json()
+
+
+def get_mets(ssPackages):
+    """
+    request METS files from Archivematica AIP packages
+    """
+
+    for package in ssPackages["objects"]:
+        # only scan AIP packages, ignore replicated and deleted packages
+        if (
+            package["package_type"] == "AIP"
+            and package["replicated_package"] is None
+            and package["status"] != "DELETED"
+        ):
+            # build relative path to METS file
+            if package["current_path"].endswith(".7z"):
+                relative_path = package["current_path"][40:-3]
+            else:
+                relative_path = package["current_path"][40:]
+            relative_path_to_mets = (
+                relative_path + "/data/METS." + package["uuid"] + ".xml"
+            )
+
+            # request METS file
+            mets_response = requests.get(
+                baseUrl
+                + apiCommand
+                + package["uuid"]
+                + "/extract_file/?relative_path_to_file="
+                + relative_path_to_mets
+                + "&username="
+                + username
+                + "&api_key="
+                + apiKey
+            )
+
+            # save METS files to disk
+            filename = package["uuid"] + ".xml"
+            with open("downloads/" + timestampStr + "/" + filename, "wb") as file:
+                file.write(mets_response.content)
+
+            # count number of actual AIP METS files (versus packages) downloaded
+            global totalAIPs
+            totalAIPs += 1
+    return totalAIPs
+
+
+# create "downloads/" directory if it doesn't exist
+if not os.path.exists("downloads/"):
+    os.makedirs("downloads/")
+
+# create a subdirectory for each download job using a timestamp as its name
+dateTimeObj = datetime.now()
+timestampStr = dateTimeObj.strftime("%Y-%m-%d--%H:%M:%S")
+os.makedirs("downloads/" + timestampStr + "/")
+
+# initial packages request
+firstPackages = get_packages(nextUrl=None)
+
+# output basic request information to user
+print("base URL: " + baseUrl)
+print("total number of packages: " + str(firstPackages["meta"]["total_count"]))
+print("download limit: " + limit)
+
+# initial METS request
+get_mets(firstPackages)
+
+print("AIP METS downloaded: " + str(totalAIPs))
+nextUrl = firstPackages["meta"]["next"]
+
+# iterate over all packages in the Archivematica Storage Service
+while nextUrl is not None:
+    print("next URL: " + nextUrl)
+    ssPackages = get_packages(nextUrl)
+    get_mets(ssPackages)
+    print("AIP METS downloaded: " + str(totalAIPs))
+    nextUrl = ssPackages["meta"]["next"]
+
+# write download summary information to a file
+with open("downloads/" + timestampStr + "/_download_info.txt", "w") as download_info:
+    download_info.write("base URL of Archivematica Storage Service: " + baseUrl + "\n")
+    download_info.write(
+        "total number of packages in Storage Service: "
+        + str(firstPackages["meta"]["total_count"])
+        + "\n"
+    )
+    download_info.write("total number of AIP METS downloaded: " + str(totalAIPs) + "\n")
+    download_info.write("download start time: " + timestampStr + "\n")
+    nowdateTimeObj = datetime.now()
+    nowtimestampStr = nowdateTimeObj.strftime("%Y-%m-%d--%H:%M:%S" + "\n")
+    download_info.write("download finish time: " + nowtimestampStr)
+    download_info.close()
---
+++ AIPscan/CLI_mets_2_json.py
@@ -0,0 +1,112 @@
+__author__ = "Peter Van Garderen"
+__email__ = "peter@artefactual.com"
+
+import os
+import metsrw
+import json
+import xml.etree.ElementTree as ET
+
+downloadDirectory = ""
+aipFilesInfo = {}
+
+with os.scandir(downloadDirectory) as dir:
+    for file in dir:
+        if file.name.endswith(".xml") and file.is_file():
+            mets = metsrw.METSDocument.fromfile(file.path)
+            aipUuid = file.name[:-4]
+
+            # metsrw library does not give access to original Transfer Name
+            # which is often more useful to end-users than the AIP uuid
+            # so we'll take the extra processing hit here to retrieve it
+            metsTree = ET.parse(file)
+            dmdSec1 = metsTree.find("{http://www.loc.gov/METS/}dmdSec[@ID='dmdSec_1']")
+            for element in dmdSec1.getiterator():
+                if element.tag == "{http://www.loc.gov/premis/v3}originalName":
+                    originalName = element.text
+                    break
+            transferName = originalName[:-37]
+
+            for aipFile in mets.all_files():
+
+                if aipFile.use == "original":
+                    aipFilesInfo[aipFile.file_uuid] = []
+                    aipFilesInfo[aipFile.file_uuid].append(
+                        {
+                            "file name": aipFile.label,
+                            "file type": "original",
+                            "transfer name": transferName,
+                            "AIP uuid": aipUuid,
+                        }
+                    )
+                    # this exception handler had to be added because METSRW throws
+                    # the error: "metsrw/plugins/premisrw/premis.py", line 644, in _to_colon_ns
+                    # parts = [x.strip("{") for x in bracket_ns.split("}")]"
+                    # AttributeError: 'cython_function_or_method' object has no attribute 'split'
+                    # for .iso file types
+                    try:
+                        for premis_object in aipFile.get_premis_objects():
+                            # it would be nice if we could access premis_object.original_name
+                            # this would allow us to parse a human readable name for the AIP
+                            aipFilesInfo[aipFile.file_uuid].append(
+                                {
+                                    "file size": str(premis_object.size) + " bytes",
+                                    "puid": str(premis_object.format_registry_key),
+                                    "file format": str(premis_object.format_name),
+                                }
+                            )
+                            if (
+                                str(premis_object.format_version)
+                                != "(('format_version',),)"
+                            ):
+                                aipFilesInfo[aipFile.file_uuid].append(
+                                    {
+                                        "file format version": str(
+                                            premis_object.format_version
+                                        )
+                                    }
+                                )
+                    except:
+                        pass
+                    for premis_event in aipFile.get_premis_events():
+                        if (str(premis_event.event_type)) == "ingestion":
+                            aipFilesInfo[aipFile.file_uuid].append(
+                                {"ingestion date": premis_event.event_date_time}
+                            )
+                    if str(premis_object.related_object_identifier_value) != "()":
+                        aipFilesInfo[aipFile.file_uuid].append(
+                            {
+                                "preservation copy uuid": str(
+                                    premis_object.related_object_identifier_value
+                                )
+                            }
+                        )
+
+            for aipFile in mets.all_files():
+                if aipFile.use == "preservation":
+                    aipFilesInfo[aipFile.file_uuid] = []
+                    aipFilesInfo[aipFile.file_uuid].append(
+                        {
+                            "file name": aipFile.label,
+                            "file type": "preservation copy",
+                            "transfer name": transferName,
+                            "AIP uuid": aipUuid,
+                        }
+                    )
+                    for premis_object in aipFile.get_premis_objects():
+                        aipFilesInfo[aipFile.file_uuid].append(
+                            {
+                                "file size": str(premis_object.size) + " bytes",
+                                "normalization date": premis_event.event_date_time,
+                                "file format": str(premis_object.format_name),
+                            }
+                        )
+                        if (
+                            str(premis_object.format_version)
+                            != "(('format_version',),)"
+                        ):
+                            aipFilesInfo[aipFile.file_uuid].append(
+                                {"version: " + str(premis_object.format_version)}
+                            )
+
+with open(downloadDirectory + "/_aip-file-info.json", "w") as json_file:
+    json.dump(aipFilesInfo, json_file, indent=4)
