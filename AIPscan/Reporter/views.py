f5eedf75e59304221daa75b74e39c4c88e806292
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-08-17 15:51:57 -0400

    Add field mapping for human readability

--- AIPscan/Reporter/views.py
+++ AIPscan/Reporter/views.py
@@ -21,6 +21,33 @@ from datetime import datetime, timedelta
 reporter = Blueprint("reporter", __name__, template_folder="templates")
 
 
+def translate_headers(headers):
+    """Translate headers from something machine readable to something
+    more user friendly and translatable.
+    """
+    field_lookup = {
+        data.FIELD_AIP_NAME: "AIP Name",
+        data.FIELD_AIPS: "AIPs",
+        data.FIELD_AIP_SIZE: "Aip Size",
+        data.FIELD_ALL_AIPS: "All Aips",
+        data.FIELD_COUNT: "Count",
+        data.FIELD_CREATED_DATE: "Created Date",
+        data.FIELD_DERIVATIVE_COUNT: "Derivative Count",
+        data.FIELD_DERIVATIVE_FORMAT: "Derivative Format",
+        data.FIELD_DERIVATIVE_UUID: "Derivative UUID",
+        data.FIELD_FILE_COUNT: "File Count",
+        data.FIELD_FORMATS: "Formats",
+        data.FIELD_NAME: "Name",
+        data.FIELD_ORIGINAL_UUID: "Original UUID",
+        data.FIELD_ORIGINAL_FORMAT: "Original Format",
+        data.FIELD_RELATED_PAIRING: "Related Pairing",
+        data.FIELD_STORAGE_NAME: "Storage Service Name",
+        data.FIELD_TRANSFER_NAME: "Transfer Name",
+        data.FIELD_VERSION: "Version",
+    }
+    return [field_lookup.get(header, header) for header in headers]
+
+
 @reporter.route("/view_aips/", methods=["GET"])
 @reporter.route("/view_aips/<id>", methods=["GET"])
 def view_aips(id=0):
@@ -410,7 +437,7 @@ def aip_contents():
         storage_service=storage_service_id,
         storage_service_name=storage_service_name,
         aip_data=aip_data,
-        columns=headers,
+        columns=translate_headers(headers),
         rows=rows,
         format_lookup=format_lookup,
     )
@@ -453,6 +480,6 @@ def original_derivatives():
         storage_service=storage_service_id,
         storage_service_name=storage_service_name,
         aip_count=aip_count,
-        headers=headers,
+        headers=translate_headers(headers),
         tables=tables,
     )
