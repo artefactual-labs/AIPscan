ac51f6c0b40af6e3f25dee40465dc6750aa564a4
Tessa Walsh <tessa@bitarchivist.net>
2021-05-06 17:38:15 -0400

    Obfuscate API key in Storage Service edit form

--- AIPscan/Aggregator/forms.py
+++ AIPscan/Aggregator/forms.py
@@ -3,13 +3,16 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField
 from wtforms.validators import DataRequired
+from wtforms.widgets import PasswordInput
 
 
 class StorageServiceForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     url = StringField("URL", validators=[DataRequired()])
     user_name = StringField("User name", validators=[DataRequired()])
-    api_key = StringField("API key", validators=[DataRequired()])
+    api_key = StringField(
+        "API key", validators=[DataRequired()], widget=PasswordInput(hide_value=False)
+    )
     download_limit = StringField("Download limit", default="20")
     download_offset = StringField("Download offset", default="0")
     default = BooleanField("Default")
