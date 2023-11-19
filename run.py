f082ee330f2a5e0cbb11f61a3384087e66bbeb0f
Mike Cantelon <mcantelon@gmail.com>
2023-11-18 19:42:27 -0800

    test

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import os
 from AIPscan import create_app
 
 if __name__ == "__main__":
+    bob = "hey"
+    print(bob)
     config_name = os.environ.get("FLASK_CONFIG", "default")
     app = create_app(config_name)
     app.run(host="0.0.0.0")
