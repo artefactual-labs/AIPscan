c581d3359ed09f22bf46b3764c70312d564a2dc7
Mike Cantelon <mcantelon@gmail.com>
2024-02-06 14:59:56 -0800

    Proxy test

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 import os
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
 from AIPscan import create_app
 
 if __name__ == "__main__":
     config_name = os.environ.get("FLASK_CONFIG", "default")
     app = create_app(config_name)
+
+    # App is behind one proxy that sets the -For and -Host headers.
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_proto=1, x_port=1)
+
     app.run(host="0.0.0.0")
