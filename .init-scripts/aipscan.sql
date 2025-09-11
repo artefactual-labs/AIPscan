4d7201237173c11f7a7625e26a3d8a2fd64d12ec
Jesús García Crespo <jesus@sevein.com>
2025-09-11 09:43:33 +0200

    Use utf8mb4 character set and collation
    This commit updates the MySQL configuration to use the `utf8mb4` character set
    for full Unicode support. The database creation script and the Docker Compose
    server command are both updated to use `utf8mb4` and the `utf8mb4_unicode_ci`
    collation for consistency.

--- .init-scripts/aipscan.sql
+++ .init-scripts/aipscan.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE aipscan CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE DATABASE celery CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE aipscan CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE celery CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL ON aipscan.* TO 'aipscan'@'%';
 GRANT ALL ON celery.* TO 'aipscan'@'%';
