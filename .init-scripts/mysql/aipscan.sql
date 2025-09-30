30f5e3a995c6c99c9190b4578df93e7cac1f0dc0
Jesús García Crespo <jesus@sevein.com>
2025-09-30 07:45:03 +0200

    Update percona/percona-server:8.4.6-6.1

--- .init-scripts/mysql/aipscan.sql
+++ .init-scripts/mysql/aipscan.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE aipscan CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE celery CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON aipscan.* TO 'aipscan'@'%';
-GRANT ALL ON celery.* TO 'aipscan'@'%';
+CREATE DATABASE aipscan CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE celery CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE USER 'aipscan'@'%' IDENTIFIED WITH caching_sha2_password BY 'demo';
+GRANT ALL PRIVILEGES ON aipscan.* TO 'aipscan'@'%';
+GRANT ALL PRIVILEGES ON celery.*  TO 'aipscan'@'%';
