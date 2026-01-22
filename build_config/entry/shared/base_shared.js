0ac28132c74997d045cebf93e6d185e6f767e893
Jesús García Crespo <jesus@sevein.com>
2026-01-22 19:46:45 +0100

    Drop jQuery UI datepicker for native date inputs

--- build_config/entry/shared/base_shared.js
+++ build_config/entry/shared/base_shared.js
@@ -1,9 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import "jquery-ui-dist/jquery-ui.min.css";
 
 import $ from "jquery";
-import "jquery-ui-dist/jquery-ui.min.js";
 
 window.$ = window.jQuery = $;
 
